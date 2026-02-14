61aee419d8fb4dc7fd403ed9b4114c9b9bc74ddf
manishjangra761 <manishjangra761@gmail.com>
2026-02-14 14:17:42 +0530

    enhance hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -8,10 +8,11 @@ const HeroSection = () => {
             <div className='hero-text-heading'>
                 <h1>Boost Your Typing Skills with <span className='hero-text-highlight'>Real Audio Tests</span></h1>
                 <p>Practice typing from real exam-style audio to enhance speed, accuracy, confidence</p>
+                <button>Start Practice</button>
             </div>
             {/* <img alt="hero image" /> */}
             {/* <img src={HeroImage} alt="hero image" /> */}
-            <div className='hero-text'>
+            <div className='hero-text overlay'>
                 <h2>Why Audio Typing ?</h2>
                 <li>Real exam-like audio typing practice</li>
                 <li>Improves typing speed and accuracy</li>
